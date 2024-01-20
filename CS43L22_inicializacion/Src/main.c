42c8db3eb28bdbf12864f24f7322a6c6edcc0dd6
davidpintollorente <80397735+davidpintollorente@users.noreply.github.com>
2024-01-20 18:06:21 +0100

    Add files via upload

--- CS43L22_inicializacion/Src/main.c
+++ CS43L22_inicializacion/Src/main.c
@@ -65,7 +65,93 @@ static void MX_I2S3_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+	void CS43L22_Init(){
 
+		 //POWER CONTROL 1
+		  estado[0]=0x02;
+		  estado[1]=0x01; //Power down 0x01 / 0x9E para encender
+		  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+
+		  //POWER CONTROL 2
+		  estado[0]=0x04;
+		  estado[1]=0xAF; //Encendidos auriculares A y B y altavoz apagado
+		  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+
+		  //CLOCKING CONTROL
+		  estado[0]=0x05;
+		  estado[1]=0x80; //Detección automática de reloj
+		  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+
+		  //INTERFACE CONTROL 1
+		  estado[0]=0x06;
+		  // Como el bit 5 esta reservaso hay que conocer su estado
+		  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_read,&estado[0],1,HAL_MAX_DELAY);
+		  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_read,&estado[1],1,HAL_MAX_DELAY);
+		  	  estado[1] &= (1 << 5);   // Pone los bit a 0 menos el bit reservado
+		  	  estado[1] &= ~(1 << 7);  // Modo esclavo
+		  	  estado[1] &= ~(1 << 6);  // Polaridad del reloj no invertida
+		  	  estado[1] &= ~(1 << 4);  // Modo DSP OFF
+		  	  estado[1] &= ~(1 << 2);  // Left justified, up to 24 bit (default)
+		  	  estado[1] |= (1 << 2);
+		  	  estado[1] |= (3 << 0);  // Longitud de palabra de 16 bots para I2S
+		  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+
+		  //PASSTHROUGH A
+		  estado[0]=0x08;
+		  // Como hay bits reservados hay que conocer su estado
+		  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_read,&estado[0],1,HAL_MAX_DELAY);
+		  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_read,&estado[1],1,HAL_MAX_DELAY);
+		  	  estado[1] &= 0xF0;     // Bit [7-4] reservados y el resto a 0
+		  	  estado[1] |= (1 << 0); // canal de entrada AIN1A
+		  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+
+
+		  //PASSTHROUGH B
+		   estado[0]=0x09;
+		   // Como hay bits reservados hay que conocer su estado
+		   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_read,&estado[0],1,HAL_MAX_DELAY);
+		   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_read,&estado[1],1,HAL_MAX_DELAY);
+		   	  estado[1] &= 0xF0;     // Bit [7-4] reservados y el resto a 0
+		   	  estado[1] |= (1 << 0); // canal de entrada AIN1B
+		   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+
+		   //MISCELLANEOUS CONTROLS
+		   estado[0]=0x0E;
+		   estado[1]=0x02; //Para modo de salida I2S//para modo analogico mirar la hoja tecnica
+		   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+
+		   //PLAYBACK_CONTROL 2
+		   estado[0]=0x0F;
+		   estado[1]=0x00; //Desmutea los auriculares y el altavoz
+		   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+
+		   //ESTABLECER VOLUMEN A 0 DB
+		   estado[1]=0x00;
+		   estado[0]=0x14;//PASSTHROUHG A VOLUMEN
+		   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+
+		   estado[0]=0x15;//PASSTHROUHG B VOLUMEN
+		   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+
+
+		   estado[0]=0x1A;//PCM A VOLUMEN
+		   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+
+		   estado[0]=0x1B;//PCM B VOLUMEN
+		   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+	}
+
+	void CS43L22_ON(){
+		  estado[0]=0x02;
+		  estado[1]=0x9E;
+		  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+	}
+
+	void CS43L22_OFF(){
+	      estado[0]=0x02;
+		  estado[1]=0x01;
+		  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
+	}
 /* USER CODE END 0 */
 
 /**
@@ -100,108 +186,21 @@ int main(void)
   MX_I2S3_Init();
   /* USER CODE BEGIN 2 */
 
-  //POWER CONTROL 1
-  estado[0]=0x02;
-  estado[1]=0x9E; //Power down 0x01 / 0x9E para encender
-  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-  //POWER CONTROL 2
-  estado[0]=0x04;
-  estado[1]=0xAF; //Encendidos auriculares A y B y altavoz apagado
-  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-  //CLOCKING CONTROL
-  estado[0]=0x05;
-  estado[1]=0x80; //Detección automática de reloj
-  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-  //INTERFACE CONTROL 1
-  estado[0]=0x06;
-  // Como el bit 5 esta reservaso hay que conocer su estado
-  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_read,&estado[0],1,HAL_MAX_DELAY);
-  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_read,&estado[1],1,HAL_MAX_DELAY);
-  	  estado[1] &= (1 << 5);   // Pone los bit a 0 menos el bit reservado
-  	  estado[1] &= ~(1 << 7);  // Modo esclavo
-  	  estado[1] &= ~(1 << 6);  // Polaridad del reloj no invertida
-  	  estado[1] &= ~(1 << 4);  // Modo DSP OFF
-  	  estado[1] &= ~(1 << 2);  // Left justified, up to 24 bit (default)
-  	  estado[1] |= (1 << 2);
-  	  estado[1] |= (3 << 0);  // Longitud de palabra de 16 bots para I2S
-  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-  //PASSTHROUGH A
-  estado[0]=0x08;
-  // Como hay bits reservados hay que conocer su estado
-  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_read,&estado[0],1,HAL_MAX_DELAY);
-  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_read,&estado[1],1,HAL_MAX_DELAY);
-  	  estado[1] &= 0xF0;      // Bit [7-4] reservados y el resto a 0
-  	  estado[1] |= (1 << 0); // canal de entrada AIN1A
-  HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-
-  //PASSTHROUGH B
-   estado[0]=0x09;
-   // Como hay bits reservados hay que conocer su estado
-   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_read,&estado[0],1,HAL_MAX_DELAY);
-   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_read,&estado[1],1,HAL_MAX_DELAY);
-   	  estado[1] &= 0xF0;      // Bit [7-4] reservados y el resto a 0
-   	  estado[1] |= (1 << 0); // canal de entrada AIN1B
-   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-   //MISCELLANEOUS CONTROLS
-   estado[0]=0x0E;
-   estado[1]=0x02; //Para modo de salida I2S//para modo analogico mirar la hoja tecnica
-   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-   //PLAYBACK_CONTROL 2
-   estado[0]=0x0F;
-   estado[1]=0x00; //Desmutea los auriculares y el altavoz
-   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-   //ESTABLECER VOLUMEN A 0 DB
-   estado[1]=0x00;
-   estado[0]=0x14;//PASSTHROUHG A VOLUMEN
-   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-   estado[0]=0x15;//PASSTHROUHG B VOLUMEN
-   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-
-   estado[0]=0x1A;//PCM A VOLUMEN
-   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-   estado[0]=0x1B;//PCM B VOLUMEN
-   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-
-   //EMPEZAR AUDIO DAC
-   estado[0]=0x00;
-   estado[1]=0x99;
-   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-   estado[0]=0x47;
-   estado[1]=0x80;
-   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-   estado[0]=0x00;
-   estado[1]=0x99;
-   HAL_I2C_Master_Transmit(&hi2c1,audio_addr_write,estado,2,HAL_MAX_DELAY);
-
-
-
+  CS43L22_Init();
 
 
   for (int i=0;i<1024;i++){
 	  audio[i]=rand() & 0xFFFF;
   }
 
+  CS43L22_ON();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  HAL_I2S_Transmit(&hi2s3, (uint16_t *)audio, 1024 , HAL_MAX_DELAY);
+	HAL_I2S_Transmit(&hi2s3, (uint16_t *)audio, 1024 , HAL_MAX_DELAY);
 
     /* USER CODE END WHILE */
 
