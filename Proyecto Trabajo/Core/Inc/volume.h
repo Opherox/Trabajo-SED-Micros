07ae522227e96ac565d3462edf08cfba4a3bda6d
Fernando <79937164+Fer014@users.noreply.github.com>
2024-01-21 04:11:16 +0100

    added volume.h

---
+++ Proyecto Trabajo/Core/Inc/volume.h
@@ -0,0 +1,15 @@
+/*
+ * volume.h
+ *
+ *  Created on: Jan 21, 2024
+ *      Author: ferna
+ */
+
+#ifndef INC_VOLUME_H_
+#define INC_VOLUME_H_
+#include "stm32f4xx_hal.h"
+
+void oled_Init();
+uint16_t changeVolume(ADC_HandleTypeDef hadc1, I2C_HandleTypeDef hi2c1);
+
+#endif /* INC_VOLUME_H_ */
